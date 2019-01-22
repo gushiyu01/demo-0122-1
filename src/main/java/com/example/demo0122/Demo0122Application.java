d3173422a9176a7b3efcada0389cac5d298f558c
gushiyu <923686643@qq.com>
2019-01-22 09:46:06 +0800

    Initial commit

---
+++ src/main/java/com/example/demo0122/Demo0122Application.java
@@ -0,0 +1,14 @@
+package com.example.demo0122;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo0122Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo0122Application.class, args);
+    }
+
+}
+
